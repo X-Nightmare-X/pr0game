63a89863a0c4fc1938ff768e0fee2cb99f5d49fc
Hyman <achimalexander@freenet.de>
2022-01-09 22:56:12 +0100

    #8: Heavy bug: Intransparent ingame simulator vs real combat system / Schwerwiegender Fehler: Nicht transparenter ingame Kampfsimulator vs echtes Kampfsystem
    Fixes #8
    Fixes #11
    
    EN:
    Shield and armor technologies were also swapped in the combat simulator.
    Due issue still needs to be checked.
    The simulator uses the same functions/battle engines as the real
    battles.
    Here, a random target is always chosen when shooting. Thus different
    results come out. For an average you have to simulate several times.
    
    DE:
    Schild- und Rüstungstechnologien wurden auch im Kampfsimulator
    vertauscht.
    Due Ausgabe muss noch geprüft werden.
    Der Simulator verwendet die gleichen Funktionen/Battle-Engines wie die
    richtigen Kämpfe.
    Hierbei wird beim Schießen immer ein Zufallsziel gewählt. Somit kommen
    unterschiedliche Ergebnisse heraus. Für einen Durchschnitt muss mehrfach
    Simuliert werden.

--- includes/pages/game/ShowBattleSimulatorPage.class.php
+++ includes/pages/game/ShowBattleSimulatorPage.class.php
@@ -56,9 +56,9 @@ class ShowBattleSimulatorPage extends AbstractGamePage
 				$attacker['player']				= array(
 					'id' => (1000 + $BattleSlotID + 1),
 					'username'	=> $LNG['bs_atter'].' Nr.'.($BattleSlotID + 1),
-					'military_tech' => $BattleSlot[0][109],
-					'defence_tech' => $BattleSlot[0][110],
-					'shield_tech' => $BattleSlot[0][111],
+				    'military_tech' => $BattleSlot[0][109],
+				    'shield_tech' => $BattleSlot[0][110],
+					'defence_tech' => $BattleSlot[0][111],
 					'dm_defensive' => 0,
 					'dm_attack' => 0
 				); 
@@ -98,9 +98,9 @@ class ShowBattleSimulatorPage extends AbstractGamePage
 				$defender['player']				= array(
 					'id' => (2000 + $BattleSlotID + 1),
 					'username'	=> $LNG['bs_deffer'].' Nr.'.($BattleSlotID + 1),
-					'military_tech' => $BattleSlot[1][109],
-					'defence_tech' => $BattleSlot[1][110],
-					'shield_tech' => $BattleSlot[1][111],
+				    'military_tech' => $BattleSlot[1][109],
+				    'shield_tech' => $BattleSlot[1][110],
+					'defence_tech' => $BattleSlot[1][111],
 					'dm_attack' => 0,
 					'dm_defensive' => 0,
 				); 
