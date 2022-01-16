2d81c1e91f91b12152e25b265390ba31269a2b23
Hyman <achimalexander@freenet.de>
2022-01-16 18:05:05 +0100

    #8: Heavy bug: Intransparent ingame simulator vs real combat system / Schwerwiegender Fehler: Nicht transparenter ingame Kampfsimulator vs echtes Kampfsystem
    Fixes #8
    
    In calculateAttack in the loop queried array matched by name only with
    the simulator.
    Array name in calculateAttack and simulator adapted to missions and
    array adapted to missions, because here the FleetID is never 0.
    
    Now the same eninge is used for both.
    
    
    In calculateAttack im Loop abgefragtes array stimmte Namentlich nur mit
    dem Simulator überein.
    Arrayname im calculateAttack und Simulator an Missionen angepasst und
    Array auf Missionen angepasst, da hier die FleetID nie 0 ist.
    
    Nun wird bei beiden die selbe Eninge verwendet.

--- includes/classes/missions/functions/SteemNova_Array.php
+++ includes/classes/missions/functions/SteemNova_Array.php
@@ -112,7 +112,7 @@ function shoot(&$attackers, $fleetID, $element, $unit, &$defenders, &$ad)
 		else
 		{
 			//-penetration
-			$ad['shield'] -= $penetration;
+		    $ad['shield'] += $unit['att'];
 			$victimShip['shield'] -= $unit['att']; // shoot at shield
 		}
 	}
