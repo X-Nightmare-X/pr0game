7b7e18b4308a223d16c4e187ed1034d7ef5e99b9
AxelFLOSS <axelfloss@noreply.example.org>
2022-03-24 15:08:41 +0100

    Removed factors from steal calculation

--- includes/classes/missions/functions/calculateSteal.php
+++ includes/classes/missions/functions/calculateSteal.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- *  2Moons 
+ *  2Moons
  *   by Jan-Otto Kröpke 2009-2016
  *
  * For the full copyright and license information, please view the LICENSE
@@ -16,79 +16,77 @@
  */
 
 function calculateSteal($attackFleets, $defenderPlanet, $simulate = false)
-{	
+{
 	// See: http://www.owiki.de/Beute
 	global $pricelist, $resource;
-	
+
 	$firstResource	= 901;
 	$secondResource	= 902;
 	$thirdResource	= 903;
-	
+
 	$SortFleets 	= array();
 	$capacity  	= 0;
-	
+
 	$stealResource	= array(
 		$firstResource => 0,
 		$secondResource => 0,
 		$thirdResource => 0
 	);
-	
+
 	foreach($attackFleets as $FleetID => $Attacker)
 	{
 		$SortFleets[$FleetID]		= 0;
-		
-		foreach($Attacker['unit'] as $Element => $amount)	
+
+		foreach($Attacker['unit'] as $Element => $amount)
 		{
 		    if ($Element != 210) { //Exclude spy drone from raid capacity
 		        $SortFleets[$FleetID]		+= $pricelist[$Element]['capacity'] * $amount;
 		    }
 		}
-		
-		$SortFleets[$FleetID]	*= (1 + $Attacker['player']['factor']['ShipStorage']);
-		
+
 		$SortFleets[$FleetID]	-= $Attacker['fleetDetail']['fleet_resource_metal'];
 		$SortFleets[$FleetID]	-= $Attacker['fleetDetail']['fleet_resource_crystal'];
 		$SortFleets[$FleetID]	-= $Attacker['fleetDetail']['fleet_resource_deuterium'];
 		$capacity				+= $SortFleets[$FleetID];
 	}
-	
+
 	$AllCapacity		= $capacity;
 	if($AllCapacity <= 0)
 	{
 		return $stealResource;
 	}
-	
+
 	// Step 1
 	$stealResource[$firstResource]		= min($capacity / 3, $defenderPlanet[$resource[$firstResource]] / 2);
 	$capacity	-= $stealResource[$firstResource];
-	 
+
 	// Step 2
 	$stealResource[$secondResource] 	= min($capacity / 2, $defenderPlanet[$resource[$secondResource]] / 2);
 	$capacity	-= $stealResource[$secondResource];
-	 
+
 	// Step 3
 	$stealResource[$thirdResource] 		= min($capacity, $defenderPlanet[$resource[$thirdResource]] / 2);
 	$capacity	-= $stealResource[$thirdResource];
-		 
+
 	// Step 4
 	$oldMetalBooty  					= $stealResource[$firstResource];
 	$stealResource[$firstResource] 		+= min($capacity / 2, $defenderPlanet[$resource[$firstResource]] / 2 - $stealResource[$firstResource]);
 	$capacity	-= $stealResource[$firstResource] - $oldMetalBooty;
-		 
+
 	// Step 5
 	$stealResource[$secondResource] 	+= min($capacity, $defenderPlanet[$resource[$secondResource]] / 2 - $stealResource[$secondResource]);
-			
+
 	if($simulate)
 	{
 		return $stealResource;
 	}
-	
+
 	$db	= Database::get();
 
 	foreach($SortFleets as $FleetID => $Capacity)
 	{
 		$slotFactor	= $Capacity / $AllCapacity;
-		
+
 		$sql	= "UPDATE %%FLEETS%% SET
 		`fleet_resource_metal` = `fleet_resource_metal` + '".($stealResource[$firstResource] * $slotFactor)."',
 		`fleet_resource_crystal` = `fleet_resource_crystal` + '".($stealResource[$secondResource] * $slotFactor)."',
@@ -99,7 +97,6 @@ function calculateSteal($attackFleets, $defenderPlanet, $simulate = false)
 			':fleetId'	=> $FleetID,
 	  	));
 	}
-	
+
 	return $stealResource;
 }
-	
