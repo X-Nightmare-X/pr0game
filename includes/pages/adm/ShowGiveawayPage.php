91265f682abd2ccc109b7bdfe6c517af120e86ff
AxelFLOSS <axelfloss@noreply.example.org>
2022-04-05 00:51:58 +0200

    Fix code style and minor refactoring for ShowGiveawayPage

--- includes/pages/adm/ShowGiveawayPage.php
+++ includes/pages/adm/ShowGiveawayPage.php
@@ -15,76 +15,82 @@
  * @link https://github.com/jkroepke/2Moons
  */
 
- if (!allowedTo(str_replace(array(dirname(__FILE__), '\\', '/', '.php'), '', __FILE__))) throw new Exception("Permission error!");
+if (!allowedTo(str_replace([dirname(__FILE__), '\\', '/', '.php'], '', __FILE__))) {
+    throw new Exception("Permission error!");
+}
 function ShowGiveaway()
 {
-	global $LNG, $resource, $reslist;
-	$template	= new template();
-	$action	= HTTP::_GP('action', '');
-	if ($action == 'send') {
-		$planet			= HTTP::_GP('planet', 0);
-		$moon			= HTTP::_GP('moon', 0);
-		$mainplanet		= HTTP::_GP('mainplanet', 0);
-		$no_inactive	= HTTP::_GP('no_inactive', 0);
+    global $LNG, $resource, $reslist;
+    $template = new template();
+    $action = HTTP::_GP('action', '');
+    if ($action == 'send') {
+        $planet = HTTP::_GP('planet', 0);
+        $moon = HTTP::_GP('moon', 0);
+        $mainplanet = HTTP::_GP('mainplanet', 0);
+        $no_inactive = HTTP::_GP('no_inactive', 0);
 
-		if (!$planet && !$moon) {
-			$template->message($LNG['ga_selectplanettype']);
-			exit;
-		}
+        if (!$planet && !$moon) {
+            $template->message($LNG['ga_selectplanettype']);
+            exit;
+        }
 
-		$planetIN	= array();
+        $planetIN = [];
 
-		if ($planet) {
-			$planetIN[]	= "'1'";
-		}
+        if ($planet) {
+            $planetIN[] = "'1'";
+        }
 
-		if ($moon) {
-			$planetIN[]	= "'3'";
-		}
+        if ($moon) {
+            $planetIN[] = "'3'";
+        }
 
-		$data		= array();
+        $data = [];
 
-		$DataIDs	= array_merge($reslist['resstype'][1], $reslist['resstype'][3], $reslist['build'], $reslist['tech'], $reslist['fleet'], $reslist['defense']);
+        $DataIDs = array_merge(
+            $reslist['resstype'][1],
+            $reslist['resstype'][3],
+            $reslist['build'],
+            $reslist['tech'],
+            $reslist['fleet'],
+            $reslist['defense']
+        );
 
-		$logOld		= array();
-		$logNew		= array();
+        $logOld = [];
+        $logNew = [];
 
-		foreach($DataIDs as $ID)
-		{
-			$amount	= max(0, round(HTTP::_GP('element_'.$ID, 0.0)));
-			$data[]	= $resource[$ID]." = ".$resource[$ID]." + ".$amount;
+        foreach ($DataIDs as $ID) {
+            $amount = max(0, round(HTTP::_GP('element_' . $ID, 0.0)));
+            $data[] = $resource[$ID] . " = " . $resource[$ID] . " + " . $amount;
 
-			$logOld[$ID]	= 0;
-			$logNew[$ID]	= $amount;
-		}
+            $logOld[$ID] = 0;
+            $logNew[$ID] = $amount;
+        }
 
-		$SQL		= "UPDATE ".PLANETS." p INNER JOIN ".USERS." u ON p.id_owner = u.id";
+        $SQL = "UPDATE " . PLANETS . " p INNER JOIN " . USERS . " u ON p.id_owner = u.id";
 
-		if ($mainplanet == true) {
-			$SQL	.= " AND p.id = u.id_planet";
-		}
+        if ($mainplanet == true) {
+            $SQL .= " AND p.id = u.id_planet";
+        }
 
-		if ($no_inactive == true) {
-			$SQL	.= " AND u.onlinetime > ".(TIMESTAMP - INACTIVE);
-		}
+        if ($no_inactive == true) {
+            $SQL .= " AND u.onlinetime > " . (TIMESTAMP - INACTIVE);
+        }
 
-		$SQL	.= " SET ".implode(', ', $data)." WHERE p.universe = ".Universe::getEmulated()." AND p.planet_type IN (".implode(',', $planetIN).")";
+        $SQL .= " SET " . implode(', ', $data) . " WHERE p.universe = " . Universe::getEmulated()
+            . " AND p.planet_type IN (" . implode(',', $planetIN) . ")";
 
-		$GLOBALS['DATABASE']->query($SQL);
+        $GLOBALS['DATABASE']->query($SQL);
 
-		$LOG = new Log(4);
-		$LOG->target = 0;
-		$LOG->old = $logOld;
-		$LOG->new = $logNew;
-		$LOG->save();
+        $LOG = new Log(4);
+        $LOG->target = 0;
+        $LOG->old = $logOld;
+        $LOG->new = $logNew;
+        $LOG->save();
 
-		$template->message($LNG['ga_success']);
-		exit;
-	}
+        $template->message($LNG['ga_success']);
+        exit;
+    }
 
-	$template->assign_vars(array(
-		'reslist'		=> $reslist
-	));
-	$template->show("giveaway.tpl");
+    $template->assign_vars(['reslist' => $reslist]);
+    $template->show("giveaway.tpl");
 }
-
