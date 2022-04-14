3d348bd2a68839c5c5ef2458484d4e50c35ed77e
AxelFLOSS <axelfloss@noreply.example.org>
2022-04-14 19:52:43 +0200

    Remove readded Dark Matter var

--- includes/classes/cache/builder/VarsBuildCache.class.php
+++ includes/classes/cache/builder/VarsBuildCache.class.php
@@ -59,7 +59,6 @@ class VarsBuildCache implements BuildCache
                     902 => $varsRow['cost902'],
                     903 => $varsRow['cost903'],
                     911 => $varsRow['cost911'],
-                    921	=> $varsRow['cost921'],
                 ],
                 'factor'        => $varsRow['factor'],
                 'max'           => $varsRow['maxLevel'],
