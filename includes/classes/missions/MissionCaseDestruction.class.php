2765e73b9ad18f3c764fbb55670c92ce01540000
Hyman <achimalexander@freenet.de>
2022-04-16 03:57:35 +0200

    Flottenverluste ergänzt und in Funktion verlagert

--- includes/classes/missions/MissionCaseDestruction.class.php
+++ includes/classes/missions/MissionCaseDestruction.class.php
@@ -626,34 +626,7 @@ HTML;
 
 	function ReturnEvent()
 	{
-		$LNG		= $this->getLanguage(NULL, $this->_fleet['fleet_owner']);
-
-		$sql		= 'SELECT name FROM %%PLANETS%% WHERE id = :planetId;';
-		$planetNameStart	= Database::get()->selectSingle($sql, array(
-			':planetId'	=> $this->_fleet['fleet_start_id'],
-		), 'name');
-
-		$sql		= 'SELECT name FROM %%PLANETS%% WHERE id = :planetId;';
-		$planetNameEnd	= Database::get()->selectSingle($sql, array(
-			':planetId'	=> $this->_fleet['fleet_end_id'],
-		), 'name');
-
-		$Message	= sprintf(
-			$LNG['sys_fleet_won'],
-			$planetNameEnd,
-			GetTargetAddressLink($this->_fleet, ''),
-			$planetNameStart,
-			GetStartAddressLink($this->_fleet, ''),
-			pretty_number($this->_fleet['fleet_resource_metal']),
-			$LNG['tech'][901],
-			pretty_number($this->_fleet['fleet_resource_crystal']),
-			$LNG['tech'][902],
-			pretty_number($this->_fleet['fleet_resource_deuterium']),
-			$LNG['tech'][903]
-		);
-
-		PlayerUtil::sendMessage($this->_fleet['fleet_owner'], 0, $LNG['sys_mess_tower'], 4, $LNG['sys_mess_fleetback'],
-			$Message, $this->_fleet['fleet_end_time'], NULL, 1, $this->_fleet['fleet_universe']);
+		$this->sendAttackReturnMessage();
 
 		$this->RestoreFleet();
 	}
