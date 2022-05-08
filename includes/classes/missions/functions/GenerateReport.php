986b8d745cf0f86a413daf912d65a04f78bf9c15
Hyman <achimalexander@freenet.de>
2022-05-08 15:31:52 +0200

    Letzte KB-Runde erzeigen

--- includes/classes/missions/functions/GenerateReport.php
+++ includes/classes/missions/functions/GenerateReport.php
@@ -112,9 +112,6 @@ function GenerateReport($combatResult, $reportInfo)
 			$DATA['rounds'][$Round]['defender'][] = $playerData;
 		}
 		
-		if ($Round >= MAX_ATTACK_ROUNDS || $Destroy['att'] == count($RoundInfo['attackers']) || $Destroy['def'] == count($RoundInfo['defenders']))
-			break;
-		
 		if(isset($RoundInfo['attack'], $RoundInfo['attackShield'], $RoundInfo['defense'], $RoundInfo['defShield']))
 			$DATA['rounds'][$Round]['info']	= array($RoundInfo['attack'], $RoundInfo['attackShield'], $RoundInfo['defense'], $RoundInfo['defShield']);
 		else
