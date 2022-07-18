9e2bda6df8c7045ff212c7500cf4093a1675c1e3
Hyman <achimalexander@freenet.de>
2022-07-18 23:04:45 +0200

    Möglicher Fix für feststeckende Flotten Nr 2

--- includes/FleetHandler.php
+++ includes/FleetHandler.php
@@ -17,20 +17,32 @@
 
 $token	= getRandomString();
 $db		= Database::get();
+$db->startTransaction();
 
-$fleetResult	= $db->update("UPDATE %%FLEETS_EVENT%% SET `lock` = :token WHERE `lock` IS NULL AND `time` <= :time;", array(
+$fleetResult = $db->select("SELECT fleetID FROM %%FLEETS_EVENT%% WHERE `lock` IS NULL AND `time` <= :time FOR UPDATE;", array(
 	':time'		=> TIMESTAMP,
-	':token'	=> $token
 ));
 
-if($db->rowCount() !== 0) {
-	require 'includes/classes/class.FlyingFleetHandler.php';
-	
-	$fleetObj	= new FlyingFleetHandler();
-	$fleetObj->setToken($token);
-	$fleetObj->run();
+if (!empty($fleetResult) && is_array($fleetResult)) {
+	$updateResult = true;
+	foreach ($fleetResult as $key => $value) {
+		$updateResult	= $updateResult && $db->update("UPDATE %%FLEETS_EVENT%% SET `lock` = :token WHERE fleetID = :id;", array(
+			':token' => $token,
+			':id' => $value['fleetID'],
+		));
+	}
 
-	$db->update("UPDATE %%FLEETS_EVENT%% SET `lock` = NULL WHERE `lock` = :token;", array(
-		':token' => $token
-	));
+	if ($updateResult) {
+		require 'includes/classes/class.FlyingFleetHandler.php';
+		
+		$fleetObj	= new FlyingFleetHandler();
+		$fleetObj->setToken($token);
+		$fleetObj->run();
+
+		$db->update("UPDATE %%FLEETS_EVENT%% SET `lock` = NULL WHERE `lock` = :token;", array(
+			':token' => $token
+		));
+	}
 }
+
+$db->commit();
