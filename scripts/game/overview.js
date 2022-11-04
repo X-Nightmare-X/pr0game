5b471f2fb4e3f3317f08935ded9df8bf0b504b89
atain <twrt75@mni.thm.de>
2022-11-04 22:56:34 +0100

    test of time on overview v3

--- scripts/game/overview.js
+++ scripts/game/overview.js
@@ -26,17 +26,7 @@ $(document).ready(function () {
     });
 
 
-    function add_static_times(){
-      const mh = document.getElementsByClassName("statictimer")
-      for (let flight of mh) {
-        let nd = new Date(parseInt(flight.getAttribute("data-time")) * 1000);
-        flight.innerText =  nd.getHours() + ":" + pad(nd.getMinutes(), 2) + ":" + pad(nd.getSeconds(), 2)
-      }
 
-
-    }
-
-    add_static_times();
 
   $('.timershort').each(function () {
     var s = $(this).data('time')
@@ -53,3 +43,14 @@ $(document).ready(function () {
 
   }, 1000);
 });
+function add_static_times(){
+  const mh = document.getElementsByClassName("statictimer")
+  for (let flight of mh) {
+    let nd = new Date(parseInt(flight.getAttribute("data-time")) * 1000);
+    flight.innerText =  nd.getHours() + ":" + pad(nd.getMinutes(), 2) + ":" + pad(nd.getSeconds(), 2)
+  }
+
+
+}
+
+add_static_times();
