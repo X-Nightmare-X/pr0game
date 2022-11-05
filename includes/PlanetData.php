19e651faeff5b21c47262a5d3c48faaa712a42bc
slippy <frosch@quak.de>
2022-11-06 00:14:51 +0100

    Revert planet data

--- includes/PlanetData.php
+++ includes/PlanetData.php
@@ -16,19 +16,19 @@
  */
 
 $planetData	= array(
-	1	=> array('temp' => mt_rand(220, 260),	'fields' => mt_rand(96, 172),	'image' => array('trocken' => mt_rand(1, 10), 'wuesten' => mt_rand(1, 4))),
-	2	=> array('temp' => mt_rand(170, 210),	'fields' => mt_rand(104, 176),	'image' => array('trocken' => mt_rand(1, 10), 'wuesten' => mt_rand(1, 4))),
-	3	=> array('temp' => mt_rand(120, 160),	'fields' => mt_rand(112, 182),	'image' => array('trocken' => mt_rand(1, 10), 'wuesten' => mt_rand(1, 4))),
-	4	=> array('temp' => mt_rand(70, 110),	'fields' => mt_rand(118, 208),	'image' => array('dschjungel' => mt_rand(1, 10))),
-	5	=> array('temp' => mt_rand(60, 100),	'fields' => mt_rand(133, 232),	'image' => array('dschjungel' => mt_rand(1, 10))),
-	6	=> array('temp' => mt_rand(50, 90),		'fields' => mt_rand(146, 242),	'image' => array('dschjungel' => mt_rand(1, 10))),
-	7	=> array('temp' => mt_rand(40, 80),		'fields' => mt_rand(152, 248),	'image' => array('normaltemp' => mt_rand(1, 7))),
-	8	=> array('temp' => mt_rand(30, 70),		'fields' => mt_rand(156, 252),	'image' => array('normaltemp' => mt_rand(1, 7))),
-	9	=> array('temp' => mt_rand(20, 60),		'fields' => mt_rand(150, 246),	'image' => array('normaltemp' => mt_rand(1, 7), 'wasser' => mt_rand(1, 9))),
-	10	=> array('temp' => mt_rand(10, 50),		'fields' => mt_rand(142, 232),	'image' => array('normaltemp' => mt_rand(1, 7), 'wasser' => mt_rand(1, 9))),
-	11	=> array('temp' => mt_rand(0, 40),		'fields' => mt_rand(136, 210),	'image' => array('normaltemp' => mt_rand(1, 7), 'wasser' => mt_rand(1, 9))),
-	12	=> array('temp' => mt_rand(-10, 30),	'fields' => mt_rand(125, 186),	'image' => array('normaltemp' => mt_rand(1, 7), 'wasser' => mt_rand(1, 9))),
-	13	=> array('temp' => mt_rand(-50, -10),	'fields' => mt_rand(114, 172),	'image' => array('eis' => mt_rand(1, 10))),
-	14	=> array('temp' => mt_rand(-90, -50),	'fields' => mt_rand(100, 168),	'image' => array('eis' => mt_rand(1, 10))),
-	15	=> array('temp' => mt_rand(-130, -90),	'fields' => mt_rand(90, 164),	'image' => array('eis' => mt_rand(1, 10)))
+	1	=> array('temp' => mt_rand(220, 260),	'fields' => mt_rand(95, 108),	'image' => array('trocken' => mt_rand(1, 10), 'wuesten' => mt_rand(1, 4))),
+	2	=> array('temp' => mt_rand(170, 210),	'fields' => mt_rand(97, 110),	'image' => array('trocken' => mt_rand(1, 10), 'wuesten' => mt_rand(1, 4))),
+	3	=> array('temp' => mt_rand(120, 160),	'fields' => mt_rand(98, 139),	'image' => array('trocken' => mt_rand(1, 10), 'wuesten' => mt_rand(1, 4))),
+	4	=> array('temp' => mt_rand(70, 110),	'fields' => mt_rand(123, 210),	'image' => array('dschjungel' => mt_rand(1, 10))),
+	5	=> array('temp' => mt_rand(60, 100),	'fields' => mt_rand(148, 215),	'image' => array('dschjungel' => mt_rand(1, 10))),
+	6	=> array('temp' => mt_rand(50, 90),		'fields' => mt_rand(148, 239),	'image' => array('dschjungel' => mt_rand(1, 10))),
+	7	=> array('temp' => mt_rand(40, 80),		'fields' => mt_rand(141, 242),	'image' => array('normaltemp' => mt_rand(1, 7))),
+	8	=> array('temp' => mt_rand(30, 70),		'fields' => mt_rand(163, 248),	'image' => array('normaltemp' => mt_rand(1, 7))),
+	9	=> array('temp' => mt_rand(20, 60),		'fields' => mt_rand(155, 243),	'image' => array('normaltemp' => mt_rand(1, 7), 'wasser' => mt_rand(1, 9))),
+	10	=> array('temp' => mt_rand(10, 50),		'fields' => mt_rand(151, 225),	'image' => array('normaltemp' => mt_rand(1, 7), 'wasser' => mt_rand(1, 9))),
+	11	=> array('temp' => mt_rand(0, 40),		'fields' => mt_rand(139, 205),	'image' => array('normaltemp' => mt_rand(1, 7), 'wasser' => mt_rand(1, 9))),
+	12	=> array('temp' => mt_rand(-10, 30),	'fields' => mt_rand(134, 180),	'image' => array('normaltemp' => mt_rand(1, 7), 'wasser' => mt_rand(1, 9))),
+	13	=> array('temp' => mt_rand(-50, -10),	'fields' => mt_rand(109, 121),	'image' => array('eis' => mt_rand(1, 10))),
+	14	=> array('temp' => mt_rand(-90, -50),	'fields' => mt_rand(81, 93),	'image' => array('eis' => mt_rand(1, 10))),
+	15	=> array('temp' => mt_rand(-130, -90),	'fields' => mt_rand(65, 74),	'image' => array('eis' => mt_rand(1, 10)))
 );
