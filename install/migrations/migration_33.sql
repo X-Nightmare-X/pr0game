656c7e6644cd64a9d7e670623c7bbf36ada8546b
Reflex <reflexrecon@pr0game.tv>
2022-11-26 02:32:17 +0100

    change deut formula

--- install/migrations/migration_33.sql
+++ install/migrations/migration_33.sql
@@ -1,3 +1,3 @@
 -- add change deut formula
 
-UPDATE `%PREFIX%vars` SET `production903` = '(10 * $BuildLevel * pow((1.1), $BuildLevel) * (-0.002 * $BuildTemp + 1.28) * (0.1 * $BuildLevelFactor))' WHERE `elementID` = 3;
+UPDATE `%PREFIX%vars` SET `production903` = '(floor(10 * $BuildLevel * pow((1.1), $BuildLevel) * (1.44 -0.004 * $BuildTemp)) * (0.1 * $BuildLevelFactor))' WHERE `elementID` = 3;
