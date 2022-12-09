75b12c11de7ed01b083eef652645424b35d1c4f3
atain <twrt75@mni.thm.de>
2022-12-09 17:03:12 +0100

    added what planets one has

--- scripts/game/galaxy.js
+++ scripts/game/galaxy.js
@@ -1,32 +1,31 @@
 function doit(missionID, planetID) {
-	$.getJSON("game.php?page=fleetAjax&ajax=1&mission="+missionID+"&planetID="+planetID, function(data)
-	{
-		$('#slots').text(data.slots);
-		if(typeof data.ships !== "undefined")
-		{
-			$.each(data.ships, function(elementID, value) {
-				$('#elementID'+elementID).text(number_format(value));
-			});
-		}
+  $.getJSON("game.php?page=fleetAjax&ajax=1&mission=" + missionID + "&planetID=" + planetID, function (data) {
+    $('#slots').text(data.slots);
+    if (typeof data.ships !== "undefined") {
+      $.each(data.ships, function (elementID, value) {
+        $('#elementID' + elementID).text(number_format(value));
+      });
+    }
 
-		var statustable	= $('#fleetstatusrow');
-		var messages	= statustable.find("~tr");
-		if(messages.length == MaxFleetSetting) {
-			messages.filter(':last').remove();
-		}
-		var element		= $('<td />').attr('colspan', 8).attr('class', data.code == 600 ? "success" : "error").text(data.mess).wrap('<tr />').parent();
-		statustable.removeAttr('style').after(element);
-	});
+    var statustable = $('#fleetstatusrow');
+    var messages = statustable.find("~tr");
+    if (messages.length == MaxFleetSetting) {
+      messages.filter(':last').remove();
+    }
+    var element = $('<td />').attr('colspan', 8).attr('class', data.code == 600 ? "success" : "error").text(data.mess).wrap('<tr />').parent();
+    statustable.removeAttr('style').after(element);
+  });
 }
 
 function galaxy_submit(value) {
-	$('#auto').attr('name', value);
-	$('#galaxy_form').submit();
+  $('#auto').attr('name', value);
+  $('#galaxy_form').submit();
 }
 
 const universe = GM_getValue("universe", {})
 const playerids = GM_getValue("playerids", {})
 const allyids = GM_getValue("allyids", {})
+
 function GM_getValue(key, defaultv) {
   let x = localStorage.getItem(key);
   if (x === null) {
@@ -40,8 +39,8 @@ function GM_setValue(key, value) {
 }
 
 
-function addlisteners (){
-  document.getElementById("gala_sync").onclick = ()=>{
+function addlisteners() {
+  document.getElementById("gala_sync").onclick = () => {
     syncsys();
     document.getElementById("gala_sync").value = "Synced";
 
@@ -50,12 +49,13 @@ function addlisteners (){
   document.getElementById('gala_upload').addEventListener('change', onChange_import);
 
 
-
-  document.getElementById("gala_exportjson").onclick =export_json
-    document.getElementById("gala_exportcsv").onclick =export_csv
-      document.getElementById("gala_reset").onclick =reset
+  document.getElementById("gala_exportjson").onclick = export_json
+  document.getElementById("gala_exportcsv").onclick = export_csv
+  document.getElementById("gala_reset").onclick = reset
+  showplayerinfos();
 }
-document.addEventListener("DOMContentLoaded", function() {
+
+document.addEventListener("DOMContentLoaded", function () {
 
   addlisteners();
 });
@@ -67,63 +67,66 @@ function onChange_import(event) {
   reader.readAsText(event.target.files[0]);
 }
 
-function onReaderLoad_import(event){
+function onReaderLoad_import(event) {
   var ttl = JSON.parse(event.target.result);
-  let obj=ttl[0]
-  let pid=ttl[1]
-  let aid=ttl[2]
-  for(let k in obj){
-    if(k in universe ){
-      if(universe[k].timepoint<obj[k].timepoint){
-        universe[k]=obj[k]
+  let obj = ttl[0]
+  let pid = ttl[1]
+  let aid = ttl[2]
+  for (let k in obj) {
+    if (k in universe) {
+      if (universe[k].timepoint < obj[k].timepoint) {
+        universe[k] = obj[k]
       }
-    }else{
-      universe[k]=obj[k]
+    } else {
+      universe[k] = obj[k]
     }
   }
-  for(let k in pid){
-    if(k in playerids ){
-      if(playerids[k].timepoint<pid[k].timepoint){
-        playerids[k]=pid[k]
+  for (let k in pid) {
+    if (k in playerids) {
+      if (playerids[k].timepoint < pid[k].timepoint) {
+        playerids[k] = pid[k]
       }
-    }else{
-      playerids[k]=pid[k]
+    } else {
+      playerids[k] = pid[k]
     }
   }
-  for(let k in aid){
-    if(k in allyids ){
-      if(allyids[k].timepoint<aid[k].timepoint){
-        allyids[k]=aid[k]
+  for (let k in aid) {
+    if (k in allyids) {
+      if (allyids[k].timepoint < aid[k].timepoint) {
+        allyids[k] = aid[k]
       }
-    }else{
-      allyids[k]=aid[k]
+    } else {
+      allyids[k] = aid[k]
     }
   }
   GM_setValue("universe", universe)
   GM_setValue("playerids", playerids)
   GM_setValue("allyids", allyids)
 }
-function export_csv(){
- let keys = Object.keys(universe)
-keys.sort(compareFn)
-  var outcsv="coords,moon?,name,ally,status,\n"
-  for(let k of keys){
-    for(let p=1;p<16;p++){
-      console.log(universe[k],k,universe[k][p]!==null,p)
-      if(universe[k][p]!==null){
+
+function export_csv() {
+  let keys = Object.keys(universe)
+  keys.sort(compareFn)
+  var outcsv = "coords,moon?,name,ally,status,\n"
+  for (let k of keys) {
+    for (let p = 1; p < 16; p++) {
+      console.log(universe[k], k, universe[k][p] !== null, p)
+      if (universe[k][p] !== null) {
         let hasmoon = universe[k][p].hasmoon ? 'X' : ''
-        outcsv+=k+":"+p + "," + hasmoon + ","+ playerids[universe[k][p].playerid].name + "("+universe[k][p].playerid + ")"+
-          ","+ allyids[universe[k][p].allianceid].name +"("+ universe[k][p].allianceid +")," + universe[k][p].special + ",\n"
+        outcsv += k + ":" + p + "," + hasmoon + "," + playerids[universe[k][p].playerid].name + "(" + universe[k][p].playerid + ")" +
+          "," + allyids[universe[k][p].allianceid].name + "(" + universe[k][p].allianceid + ")," + universe[k][p].special + ",\n"
       }
     }
   }
-  download("universe.csv",outcsv)
+  download("universe.csv", outcsv)
 }
-function export_json(){
-  download("universe.json",JSON.stringify([universe,playerids,allyids]))
+
+function export_json() {
+  download("universe.json", JSON.stringify([universe, playerids, allyids]))
 }
-function reset(){
-  if(confirm("delete data?")===false){
+
+function reset() {
+  if (confirm("delete data?") === false) {
     return;
   }
   Object.keys(universe).forEach(key => delete universe[key]);
@@ -134,39 +137,88 @@ function reset(){
   GM_setValue("allyids", allyids)
 }
 
-function syncsys(){
-  universe[galakey]=systemdata;
-  for(let pos in systemdata){
-    if(pos==="timepoint" || systemdata[pos]===null){
+function syncsys() {
+  universe[galakey] = systemdata;
+  for (let pos in systemdata) {
+    if (pos === "timepoint" || systemdata[pos] === null) {
       continue
     }
-    playerids[systemdata[pos].playerid]={name:systemdata[pos].name,timepoint:systemdata.timepoint}
-    allyids[systemdata[pos].allianceid]={name:systemdata[pos].alliancename,timepoint:systemdata.timepoint}
+    playerids[systemdata[pos].playerid] = {name: systemdata[pos].name, timepoint: systemdata.timepoint}
+    allyids[systemdata[pos].allianceid] = {name: systemdata[pos].alliancename, timepoint: systemdata.timepoint}
   }
   GM_setValue("universe", universe)
   GM_setValue("playerids", playerids)
   GM_setValue("allyids", allyids)
 
 }
+
+function showplayerinfos() {
+  for (let username of document.getElementsByClassName(" galaxy-username")) {
+    console.log(username.getAttribute("playerid"),)
+    annotatehover(username.getAttribute("playerid"), username.parentElement)
+  }
+}
+
+function annotatehover(id, htmlobject) {
+  let anostr = htmlobject.getAttribute("data-tooltip-content").replace("</table>", "")
+  anostr += "<tr><th colspan='2'>Planets</th></tr>"
+  let baseurl = location.href.split("?")[0] + "?page=galaxy&galaxy="
+  let toshow = []
+  for (let k in universe) {
+    for (let p = 1; p < 16; p++) {
+      if (universe[k][p] === null) {
+        continue
+      }
+      if (universe[k][p].playerid == id) {
+        let gala = k.split(":")[0]
+        let sys = k.split(":")[1]
+        toshow.push([gala, sys, p, universe[k][p].hasmoon ? 'M' : ''])
+      }
+    }
+  }
+  let cgala = galakey.split(":")[0]
+  let csys = galakey.split(":")[1]
+  for (let coord of toshow) {
+    if (coord[0] == cgala && coord[1] == csys) {
+      anostr += "<tr><td colspan='2' style='text-align: center'><span>" + getcoordstring(coord) + "</span></td></tr>"
+    } else {
+      if (coord[0] == cgala) {
+        anostr += "<tr><th colspan='2' style='text-align: center'><a href='" + baseurl + coord[0] + "&system=" + coord[1] + "'>" + getcoordstring(coord) + "</a></th></tr>"
+      } else {
+        anostr += "<tr><td colspan='2' style='text-align: center'><a href='" + baseurl + coord[0] + "&system=" + coord[1] + "'>" + getcoordstring(coord) + "</a></td></tr>"
+      }
+    }
+  }
+
+  htmlobject.setAttribute("data-tooltip-content", anostr + "</table>")
+}
+
+function getcoordstring(coord) {
+  let x = "[" + coord[0] + ":" + coord[1] + ":" + coord[2] + "]" + " " + coord[3]
+  return x.trim()
+}
+
+
 function compareFn(a, b) {
-  let spa=a.split(":")
-  let spb=b.split(":")
-  if (parseInt(spa[0])<parseInt(spb[0])) {
+  let spa = a.split(":")
+  let spb = b.split(":")
+  if (parseInt(spa[0]) < parseInt(spb[0])) {
     return -1;
-  }else{
-    if(spa[0]!==spb[0]){
+  } else {
+    if (spa[0] !== spb[0]) {
       return 1
     }
   }
-  if (parseInt(spa[1])<parseInt(spb[1])) {
+  if (parseInt(spa[1]) < parseInt(spb[1])) {
     return -1;
-  }else{
-    if(spa[1]!==spb[1]){
+  } else {
+    if (spa[1] !== spb[1]) {
       return 1
     }
   }
   return 0;
 }
+
 function download(filename, text) {
   var element = document.createElement('a');
   element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
@@ -188,32 +240,35 @@ function logKey(e) {
   if (!keypressRegistered) {
     console.log(e);
     const callback = {
-      "a"  : leftHandler,
-      "ArrowLeft"  : leftHandler,
-      "d"  : rightHandler,
-      "ArrowRight" : rightHandler,
-      "w"    : upHandler,
-      "ArrowUp"    : upHandler,
-      "s"  : downHandler,
-      "ArrowDown"  : downHandler,
+      "a": leftHandler,
+      "ArrowLeft": leftHandler,
+      "d": rightHandler,
+      "ArrowRight": rightHandler,
+      "w": upHandler,
+      "ArrowUp": upHandler,
+      "s": downHandler,
+      "ArrowDown": downHandler,
     }[event.key]
     callback?.()
   }
 }
 
-function leftHandler(){
+function leftHandler() {
   location.assign("javascript:galaxy_submit('systemLeft')");
   keypressRegistered = true;
 }
-function rightHandler(){
+
+function rightHandler() {
   location.assign("javascript:galaxy_submit('systemRight')");
   keypressRegistered = true;
 }
-function upHandler(){
+
+function upHandler() {
   location.assign("javascript:galaxy_submit('galaxyLeft')");
   keypressRegistered = true;
 }
-function downHandler(){
+
+function downHandler() {
   location.assign("javascript:galaxy_submit('galaxyRight')");
   keypressRegistered = true;
 }
