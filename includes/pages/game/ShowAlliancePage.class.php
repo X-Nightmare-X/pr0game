29bd0832004d258b847c7b793bc57999ac4d7cd8
Reflex <reflexrecon@pr0game.tv>
2022-12-13 14:14:58 +0100

    only show ally in current universe for diplo

--- includes/pages/game/ShowAlliancePage.class.php
+++ includes/pages/game/ShowAlliancePage.class.php
@@ -2016,9 +2016,10 @@ class ShowAlliancePage extends AbstractGamePage
 
         $diplomaticMode = HTTP::_GP('diploMode', 0);
 
-        $sql = "SELECT ally_tag,ally_name,id FROM %%ALLIANCE%% WHERE id != :allianceId ORDER BY ally_tag ASC;";
+        $sql = "SELECT ally_tag,ally_name,id FROM %%ALLIANCE%% WHERE id != :allianceId  where ally_universe = :universe ORDER BY ally_tag ASC;";
         $diplomaticAlly = $db->select($sql, [
-            ':allianceId' => $USER['ally_id']
+            ':allianceId' => $USER['ally_id'],
+            ':universe' => Universe::current()
         ]);
 
         $AllyList = [];
