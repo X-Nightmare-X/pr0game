04ba2aff428cf52f23cdf8f7cb7cd5a8767416b4
atain <twrt75@mni.thm.de>
2022-12-29 03:37:30 +0100

    second prototype

--- scripts/game/overview.js
+++ scripts/game/overview.js
@@ -40,18 +40,5 @@ $(document).ready(function () {
 
   }, 1000);
 
-  function add_static_times(){
-    const mh = document.getElementsByClassName("statictimer")
-    for (let flight of mh) {
-      let nd = new Date(new Date(parseInt(flight.getAttribute("data-time")) * 1000 ).toLocaleString('en-US', { timeZone: localtimezonestring }))
-      let ddiv=Math.floor((nd-serverTime) / (1000 * 60 * 60 * 24));
-      let tamt=""
-      if(ddiv>0){
-        tamt=ddiv + "T "
-      }
 
-      flight.innerText =tamt +   pad(nd.getHours(), 2) + ":" + pad(nd.getMinutes(), 2) + ":" + pad(nd.getSeconds(), 2)
-    }
-  }
-  add_static_times();
 });
