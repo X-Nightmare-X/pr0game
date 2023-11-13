6176e55be49b96b68a3931e4c1fb851c2540801f
timo_ka <timo_ka@posteo.us>
2023-11-13 17:07:52 +0100

    after moon_chance

--- install/migrations/migration_49.sql
+++ install/migrations/migration_49.sql
@@ -1,3 +1,3 @@
 -- added Moon Factor for Uwe spacy Moon sizing
 
-ALTER TABLE `%PREFIX%config`  ADD `moonSizeFactor` float(4,3) unsigned NOT NULL DEFAULT '1.000';
+ALTER TABLE `%PREFIX%config`  ADD `moonSizeFactor` float(4,3) unsigned NOT NULL DEFAULT '1.000' AFTER `moon_chance`;
