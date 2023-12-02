651eededdd0f4a67e415f4be4238b69a85b22281
Hyman <hyman@pr0game.com>
2023-12-02 20:58:42 +0100

    Fixes selector settings for stats page with spy dependency module

--- includes/pages/game/ShowStatisticsPage.class.php
+++ includes/pages/game/ShowStatisticsPage.class.php
@@ -32,7 +32,7 @@ class ShowStatisticsPage extends AbstractGamePage
         $who = HTTP::_GP('who', 1);
         $type = HTTP::_GP('type', 1);
         $range = HTTP::_GP('range', 1);
-        if (!isModuleAvailable(MODULE_SPYTECH_DEPENDENT_STATS)){
+        if (!isModuleAvailable(MODULE_SPYTECH_DEPENDENT_STATS) || $USER['authlevel'] > 0){
             switch ($type) {
                 case 2:
                     $Order = "fleet_rank";
@@ -260,7 +260,7 @@ class ShowStatisticsPage extends AbstractGamePage
             1 => $LNG['st_player'],
             2 => $LNG['st_alliance'],
         ];
-        if (!isModuleAvailable(MODULE_SPYTECH_DEPENDENT_STATS) || $USER['authlevel'] == 0){
+        if (!isModuleAvailable(MODULE_SPYTECH_DEPENDENT_STATS) || $USER['authlevel'] > 0){
             $Selector['type'] = [
                 1 => $LNG['st_points'],
                 2 => $LNG['st_fleets'],
