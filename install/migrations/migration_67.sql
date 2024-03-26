71be8ece6e175b92d736e2443f5f8b0ff2f49b6a
timo_ka <timo_ka@posteo.us>
2024-03-26 13:32:52 +0100

    Planet posisition basesd Ress boost
    SQL Part

---
+++ install/migrations/migration_67.sql
@@ -0,0 +1,6 @@
+-- Adds bonus production as percent value to the planet
+ALTER TABLE `%PREFIX%planets` 
+    ADD COLUMN `metal_bonus_percent` tinyint unsigned NOT NULL DEFAULT 0 AFTER `metal_max`,
+    ADD COLUMN `crystal_bonus_percent` tinyint unsigned NOT NULL DEFAULT 0 AFTER `crystal_max`,
+    ADD COLUMN `deuterium_bonus_percent` tinyint unsigned NOT NULL DEFAULT 0 AFTER `deuterium_max`;
+
